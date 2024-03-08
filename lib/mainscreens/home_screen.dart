ae1a5c6e61dcd883d7434f0d9e166c61a84a7a3c
David Onuche <onuchedavid34@gmail.com>
2024-03-08 01:46:17 +0100

    carousel slider

--- lib/mainscreens/home_screen.dart
+++ lib/mainscreens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:tabour_users/auth/auth_screen.dart';
 import 'package:tabour_users/global/global.dart';
@@ -11,6 +12,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final items = [
+    "slider/0.jpg",
+    "slider/1.jpg",
+    "slider/2.jpg",
+    "slider/3.jpg",
+    "slider/4.jpg",
+    "slider/5.jpg",
+    "slider/6.jpg",
+    "slider/7.jpg",
+    "slider/8.jpg",
+    "slider/9.jpg",
+    "slider/10.jpg",
+    "slider/11.jpg",
+    "slider/12.jpg",
+    "slider/13.jpg",
+    "slider/14.jpg",
+    "slider/15.jpg",
+    "slider/16.jpg",
+    "slider/17.jpg",
+    "slider/18.jpg",
+    "slider/19.jpg",
+    "slider/20.jpg",
+    "slider/21.jpg",
+    "slider/22.jpg",
+    "slider/23.jpg",
+    "slider/24.jpg",
+    "slider/25.jpg",
+    "slider/26.jpg",
+    "slider/27.jpg",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +67,54 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
       ),
       drawer: const UserDrawer(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                height: MediaQuery.of(context).size.height * .3,
+                width: MediaQuery.of(context).size.width,
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.8,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 2),
+                    autoPlayAnimationDuration:
+                        const Duration(milliseconds: 500),
+                    autoPlayCurve: Curves.decelerate,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.3,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                  items: items.map((index) {
+                    return Builder(builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(horizontal: 1.0),
+                        decoration: const BoxDecoration(
+                          color: Colors.black,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Image.asset(
+                            index,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      );
+                    });
+                  }).toList(),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
